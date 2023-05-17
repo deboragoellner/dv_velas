d9ddb2a0b448a5d10a85932f4f4bd3ba5b28c542
deboragoellner <deboragoellner@gmail.com>
2023-05-17 15:23:50 -0300

    menu completo e dashboard com colunas em andamento

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,50 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DV Velas</title>
+        <link rel="stylesheet" type="text/css" href="../css/estilo.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body class="antialiased">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">DV Velas</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+        <br><br>
+        <div class="container text-center">
+            <style>
+                .col {
+                border: 1px solid gray;
+                padding: 8px;
+                }
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+                .col {
+                text-align: center;
+                text-transform: uppercase;
+                color: tan;
+                font-size: 20px;
+                font-family: Aboreto;
+                }
+            </style>
+            <div class="row">
+              <div class="col">
+                Cliente
+              </div>
+              <div class="col">
+                Estoque
+              </div>
+              <div class="col">
+                Fornecedor
+              </div>
+              <div class="col">
+                Tabela leitura
+              </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,19 +42,17 @@
                     @endauth
                 </div>
             @endif
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img class="d-block w-100" src="/storage/img/vela1.avif" alt="First slide">
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src="/storage/img/vela2.avif" alt="Second slide">
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src="/storage/img/vela3.avif" alt="Third slide">
-                  </div>
-                </div>
+            <div>
+                <style>
+                    .imgmenu {
+                      width: 50%;
+                      margin-left: 25%;
+                      margin-right: 15%;
+                    }
+                </style>
+                <img class="imgmenu" src="/storage/img/vela1.avif">
             </div>
+            <br>
             <div class="valores">
             <style>
                 .valores {
@@ -66,19 +64,24 @@
                 text-align: center;
                 text-transform: uppercase;
                 color: tan;
+                font-size: 50px;
+                font-family: Aboreto;
                 }
 
                 .missao {
                 text-indent: 50px;
                 text-align: justify;
                 letter-spacing: 3px;
+                margin-left: 2%;
+                margin-right: 2%;
                 }
             </style>
-                <h1>Missão dv velas</h1>
-                <p class="missao">Nós da  DV velas prezamos por trazer aos nossos clientes, colaboradores e associados, a melhor experiência na aquisição de suas velas. Não apenas como itens decorativos, mas também como itens de valor para aqueles que se juntarem à família DV Velas.
-                <br>Estimando sempre a qualidade e elegância de nossos produtos. De forma consciente visando o cuidado com o meio ambiente.</p>
+                <h1>Missão dv velas <br></h1>
+                <p class="missao"><br>  Nós da  DV velas prezamos por trazer aos nossos clientes, colaboradores e associados, a melhor experiência na aquisição de suas velas. Não apenas como itens decorativos, mas também como itens de valor para aqueles que se juntarem à família DV Velas.
+                <br>    Estimando sempre a qualidade e elegância de nossos produtos. De forma consciente visando o cuidado com o meio ambiente. <br> <br></p>
             </div>
         </div>
+        <br>
     </body>
     <footer>
         <style>
